05d5c4a55ccc00b04cd8ab6bd389803819c7102b
kao.zhu <kao.zhu@horizon.ai>
2022-06-10 14:14:56 +0800

    chore(license): [cr_id_skip] Add apache license no_3rd_skip
    Test Plan: verified
    
    Reviewers: kairui.wang
    
    Reviewed By: kairui.wang
    
    Differential Revision: https://cr.hobot.cc/D184586

--- include/data_type/mot_data_type.hpp
+++ include/data_type/mot_data_type.hpp
@@ -1,11 +1,16 @@
-/**
- * Copyright (c) 2018 Horizon Robotics. All rights reserved.
- * @brief     snapshot_data_type header
- * @author    chao.yang
- * @email     chao01.yang@horizon.ai
- * @version   0.0.1
- * @date      2019.07.25
- */
+// Copyright (c) 2022，Horizon Robotics.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef MOTMETHOD_DATA_TYPE_MOT_DATA_TYPE_HPP_
 #define MOTMETHOD_DATA_TYPE_MOT_DATA_TYPE_HPP_
