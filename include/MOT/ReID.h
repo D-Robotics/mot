f455604fc9fedfce976f565cb23af2dd7dde58f7
kao.zhu <kao.zhu@horizon.ai>
2022-06-10 14:14:56 +0800

    chore(license): [cr_id_skip] Add apache license no_3rd_skip
    Test Plan: verified
    
    Reviewers: kairui.wang
    
    Reviewed By: kairui.wang
    
    Differential Revision: https://cr.hobot.cc/D184586

--- include/MOT/ReID.h
+++ include/MOT/ReID.h
@@ -1,11 +1,16 @@
-/**
- * Copyright (c) 2018 Horizon Robotics. All rights reserved.
- * @brief     ReID MOT header
- * @author    chao.yang
- * @email     chao01.yang@horizon.ai
- * @version   0.0.0.1
- * @date      2018.12.18
- */
+// Copyright (c) 2022，Horizon Robotics.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
 #ifndef MOTMETHOD_MOT_REID_H_
 #define MOTMETHOD_MOT_REID_H_
